89fcb017a6e04f002a1e636afa31b1b1154d8f6c
Yanina-Kutovaya <56822023+Yanina-Kutovaya@users.noreply.github.com>
2020-04-23 13:38:43 +0300

    Scrapy
    1. Доработать паука в имеющемся проекте, чтобы он формировал item по структуре:
      - наименование вакансии
      - зарплата от
      - зарплата до
      - ссылку на саму вакансию
      - сайт откуда собрана вакансия
    и складывал все записи в БД (любую)
    
    2. Создать в имеющемся проекте второго паука по сбору вакансий с сайта superjob. Паук должен формировать item'ы по аналогичной структуре и складывать данные также в БД

--- jobparser/pipelines.py
+++ jobparser/pipelines.py
@@ -7,39 +7,55 @@
 from pymongo import MongoClient
 import numpy as np
 
+
 class JobparserPipeline(object):
     def __init__(self):
         client = MongoClient('localhost', 27017)
         self.mongo_base = client.vacancies_2
+        self.adjustments = JobparserItemAdjustmentsPipeline()
 
     def process_item(self, item, spider):
         collection = self.mongo_base[spider.name]
+        item = self.adjustments.item_adjustments(item, spider)
+        collection.insert_one(item)
+        return item
+
+
+class JobparserItemAdjustmentsPipeline(object):
+    def item_adjustments(self, item, spider):
         item['min_salary'] = np.nan
         item['max_salary'] = np.nan
         item['currency'] = np.nan
-        if spider.name == 'hhru':
-            item['site'] = 'https://hh.ru/'
-            if len(item['salary']) != 0:
-                for i in range(len(item['salary'])):
-                    if item['salary'][i] == "от ":
-                        item['min_salary'] = int(item['salary'][i + 1].replace('\xa0', '').replace(' ', ''))
-                    elif (item['salary'][i] == " до ") | (item['salary'][i] == "до "):
-                        item['max_salary'] = int(item['salary'][i + 1].replace('\xa0', '').replace(' ', ''))
-                    elif (item['salary'][i] == "руб.") | (item['salary'][i] == "USD") | (item['salary'][i] == "EUR"):
-                        item['currency'] = item['salary'][i]
-        elif spider.name == 'sj':
-            item['site'] = 'https://russia.superjob.ru'
-            if len(item['salary']) >= 2:
-                if item['salary'][0] == "от":
-                    item['min_salary'] = int(item['salary'][2][:-4].replace('\xa0', '').replace(' ', ''))
-                    item['currency'] = item['salary'][2][-4:]
-                elif item['salary'][0].replace('\xa0', '').replace(' ', '').isnumeric():
-                    item['min_salary'] = int(item['salary'][0].replace('\xa0', '').replace(' ', ''))
-                    item['max_salary'] = int(item['salary'][1].replace('\xa0', '').replace(' ', ''))
-                    item['currency'] = item['salary'][3]
-
-        collection.insert_one(item)
+        if spider.name == 'sj':
+            item = self.sj_item_adjustments(item)
+        elif spider.name == 'hhru':
+            item = self.hhru_item_adjustments(item)
         return item
+
+    def sj_item_adjustments(self, item):
+        item['site'] = 'https://russia.superjob.ru'
+        if len(item['salary']) >= 2:
+            if item['salary'][0] == "от":
+                item['min_salary'] = int(item['salary'][2][:-4].replace('\xa0', '').replace(' ', ''))
+                item['currency'] = item['salary'][2][-4:]
+            elif item['salary'][0].replace('\xa0', '').replace(' ', '').isnumeric():
+                item['min_salary'] = int(item['salary'][0].replace('\xa0', '').replace(' ', ''))
+                item['max_salary'] = int(item['salary'][1].replace('\xa0', '').replace(' ', ''))
+                item['currency'] = item['salary'][3]
+        return item
+
+    def hhru_item_adjustments(self, item):
+        item['site'] = 'https://hh.ru/'
+        if len(item['salary']) != 0:
+            for i in range(len(item['salary'])):
+                if item['salary'][i] == "от ":
+                    item['min_salary'] = int(item['salary'][i + 1].replace('\xa0', '').replace(' ', ''))
+                elif (item['salary'][i] == " до ") | (item['salary'][i] == "до "):
+                    item['max_salary'] = int(item['salary'][i + 1].replace('\xa0', '').replace(' ', ''))
+                elif (item['salary'][i] == "руб.") | (item['salary'][i] == "USD") | (item['salary'][i] == "EUR"):
+                    item['currency'] = item['salary'][i]
+        return item
+
 
 
 
--- jobparser/settings.py
+++ jobparser/settings.py
@@ -71,6 +71,7 @@ COOKIES_ENABLED = True
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     'jobparser.pipelines.JobparserPipeline': 300,
+    'jobparser.pipelines.JobparserItemAdjustmentsPipeline': 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
