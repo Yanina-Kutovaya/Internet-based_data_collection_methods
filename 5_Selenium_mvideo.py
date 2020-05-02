fdeb1f691b3796c674c2dc75a3835d5457201472
Yanina-Kutovaya <56822023+Yanina-Kutovaya@users.noreply.github.com>
2020-05-02 15:18:55 +0300

    Selenium в Python

--- 5_Selenium_mvideo.py
+++ 5_Selenium_mvideo.py
@@ -2,7 +2,10 @@
 #  Магазины можно выбрать свои. Главный критерий выбора: динамически загружаемые товары
 
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+
 from pymongo import MongoClient
 from pprint import pprint
 import time
@@ -10,46 +13,40 @@ import time
 
 def parse_items(hit_sales, db):
     items = hit_sales.find_elements_by_tag_name('li')
-    n = len(db)
     for item in items:
         df = {}
-        name = item.find_elements_by_xpath(".//a[@data-product-info]")[1].text
-        price = int(item.find_elements_by_xpath(".//div[@class='c-pdp-price__current']")[0].text[:-1].replace(' ', ''))
-        df['name'] = name
-        df['price'] = price
-        db[n] = df
-        n += 1
+        df['name'] = item.find_elements_by_xpath(".//a[@data-product-info]")[1].text
+        price = item.find_elements_by_xpath(".//div[@class='c-pdp-price__current']")[0].text[:-1]
+        if df['name'] != '':
+            df['price'] = int(price.replace(' ', ''))
+            db.append(df)
     return db
 
 
-def scroll(hit_sales, driver):
-    time.sleep(3)
-    if hit_sales.find_element_by_xpath(".//a[@class='next-btn sel-hits-button-next']"):
-        action = ActionChains(driver)
-        action.move_to_element(hit_sales.find_element_by_xpath(".//a[@class='next-btn sel-hits-button-next']"))
-        action.click()
-        hit_sales = driver.find_elements_by_xpath("//div[@class='accessories-carousel-wrapper']")[2]
-        return hit_sales, driver
+def scroll(driver):
+    driver.find_elements_by_xpath("//a[@class='next-btn sel-hits-button-next']")[2].click()
+    time.sleep(5)
+    hit_sales = WebDriverWait(driver, 20).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, 'accessories-product-list')))[2]
+    return hit_sales, driver
 
 
 if __name__ == '__main__':
     driver = webdriver.Chrome()
     driver.maximize_window()
     driver.get('https://www.mvideo.ru/')
+    time.sleep(2)
+    assert "М.Видео -" in driver.title
 
-    hit_sales = driver.find_elements_by_xpath("//div[@class='accessories-carousel-wrapper']")[2]
-    db = {}
+    db = []
+    hit_sales = driver.find_elements_by_xpath("//ul[@class='accessories-product-list']")[2]
     db = parse_items(hit_sales, db)
     for i in range(3):
-        hit_sales, driver = scroll(hit_sales, driver)
+        hit_sales, driver = scroll(driver)
         db = parse_items(hit_sales, db)
     driver.quit()
     pprint(db)
 
     client = MongoClient('localhost', 27017)
     mvideo = client['mvideo']
-    db_list = []
-    for i in db:
-        db_list.append(db[i])
-    mvideo.sales_hits.insert_many([i for i in db_list])
-
+    mvideo.sales_hits.insert_many([i for i in db])
